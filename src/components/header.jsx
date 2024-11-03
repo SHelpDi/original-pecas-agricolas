e4d8bce4d67c1eae442090e52ab57691807e534f
SHelpDi <diogo@screamhelp.com.br>
2024-11-02 21:57:50 -0300

    timeZone data e hora

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,27 +5,28 @@ import { FaWhatsapp, FaSquareFacebook, FaInstagram } from "react-icons/fa6";
 
 
 export default async function Header({}) {
-  const dollarNow = await fetch('http://economia.awesomeapi.com.br/json/last/USD-BRL');
-  const dollar = await dollarNow.json();
+  /* const dollarNow = await fetch('http://economia.awesomeapi.com.br/json/last/USD-BRL');
+  const dollar = await dollarNow.json(); */
 
   const date = new Date();
+  const options = { timeZone: 'America/Sao_Paulo' };
   const semana = ['Domingo', 'Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 'Quinta-Feira', 'Sexta-Feira', 'Sábado'];
-  const diaDaSemana = semana[date.getDay()];
-  const diaDoMes = date.getDate();
+  const diaDaSemana = semana[date.getDay('pt-BR', options)];
+  const diaDoMes = date.getDate('pt-BR', options);
   const mes = date.toLocaleString('pt-BR', { month: 'long' });
-  const ano = date.getFullYear();
+  const ano = date.getFullYear('pt-BR', options);
 
-  const weatherNow = await fetch('https://api.weatherapi.com/v1/current.json?key=c84b496246834a438ae191258242905&q=cascavel-269450&lang=pt');
-  const weather = await weatherNow.json();
+  /* const weatherNow = await fetch('https://api.weatherapi.com/v1/current.json?key=c84b496246834a438ae191258242905&q=cascavel-269450&lang=pt');
+  const weather = await weatherNow.json(); */
     
   
   return (
     <>
       <header>
         <div className="topo">
-          <span>dólar hoje - R$ {dollar.USDBRL.high}</span>
+          <span>dólar hoje - R$ {/* {dollar.USDBRL.high} */}</span>
           <span>{`${diaDaSemana}, ${diaDoMes} de ${mes} de ${ano}`}</span>
-          <span>{weather.current.condition.text}</span>
+          <span>{/* {weather.current.condition.text} */}</span>
           <div className="links">
             <Link
               href="https://www.instagram.com/originalpecasagricolas/"
